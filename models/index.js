4c65f783de7974456aee0ae8bf8daff7fcf6b190
Brandon Lisonbee <blisonbee213596@gmail.com>
2023-06-23 16:35:01 -0600

    which index change

--- models/index.js
+++ models/index.js
@@ -122,27 +122,4 @@ db.users = User;
 db.students = Student;
 db.events = Event;
 
-
-const validateUser = (req, res, next) => {
-    const user = new User({
-        firstName: req.body.firstName,
-        middleName: req.body.middleName,
-        lastName: req.bdoy.lastName,
-        username: req.body.username,
-        password: req.body.password,
-        email: req.body.email,
-        phone: req.body.phone,
-        juniorParticipants: req.body.juniorParticipants,
-        adultParticipants: req.body.adultParticipants
-    });
-
-    let error = user.validateSync();
-
-    if (error) {
-        res.status(412).send({message: error.message});
-    } else {
-        next();
-    }
-}
-
-module.exports = db, validateUser;
+module.exports = db;
